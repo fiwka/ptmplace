2dfc55dbf44a86183eb9f2561012c4063bae8e5a
fiwka <dropinclass1@gmail.com>
2025-03-09 17:57:40 +0500

    feat(frontend): implement RoutePage

--- frontend/src/RoutePage.tsx
+++ frontend/src/RoutePage.tsx
@@ -3,34 +3,76 @@ import {useParams} from "react-router";
 import {useLocalStorage} from "@uidotdev/usehooks";
 import {Token} from "./user.tsx";
 import {Button, Card} from "flowbite-react";
-import {FaArrowRight, FaTrain} from "react-icons/fa";
+import {FaArrowRight, FaBus, FaPlane, FaTrain} from "react-icons/fa";
+import {useEffect, useState} from "react";
+import {callBackend} from "./ptmplace.tsx";
 
 export default function RoutePage() {
     const {from, to, mode, departure} = useParams()
     const [accessToken, _] = useLocalStorage('access_token', {} as Token)
+    const [route, setRoute] = useState(null as unknown as any[])
+    const [isRouteSet, setIsRouteSet] = useState(false)
+
+    useEffect(() => {
+        if (!isRouteSet) {
+            callBackend(
+                mode == 'mix' ?
+                    `route/${from}/${to}?mix=true&departure=${departure}` :
+                    `route/${from}/${to}?mode=${mode}&departure=${departure}`,
+                accessToken.token
+            )
+                .then(res => res.json())
+                .then(res => {
+                    const routes = []
+
+                    for (let i = 0; i < res.length; i++) {
+                        const arrival = i != res.length - 1 ? new Date(res[i + 1].arrival) : null
+                        const departure = i != res.length - 1 ? new Date(res[i + 1].departure) : null
+
+                        routes.push(
+                            <div key={i} className="flex flex-row gap-2 items-center">
+                                <h5 className="dark:text-white font-bold text-xl">{res[i].city.name}</h5>
+                                {res[i].transportMode == "PLANE" ?
+                                    <FaPlane className="dark:text-white"/> : (res[i].transportMode == "RAILWAY" ?
+                                        <FaTrain className="dark:text-white"/> :
+                                        <FaBus className="dark:text-white"/>)}
+                            </div>
+                        )
+
+                        if (i != res.length - 1) {
+                            routes.push(
+                                <div key={res.length + i} className="flex flex-col gap-0.5 items-center">
+                                    <div>
+                                        <FaArrowRight className="dark:text-white"/>
+                                    </div>
+                                    <div>
+                                        <p className="text-gray-500 text-sm">{departure?.toLocaleString()} - {arrival?.toLocaleString()}</p>
+                                    </div>
+                                </div>
+                            )
+                        }
+                    }
+
+                    setRoute(routes)
+                })
+
+            setIsRouteSet(true)
+        }
+    })
 
     return (<>
         <AppNavbar/>
         <div className="flex flex-col min-h-screen min-w-screen gap-4 items-center justify-center">
-            <h1 className="dark:text-white text-center text-3xl">Лучший подходящий маршрут</h1>
-            <Card className="min-w-max" horizontal>
-                <div className="flex flex-row gap-4 items-center">
-                    <div className="flex flex-row gap-2 items-center">
-                        <h5 className="dark:text-white font-bold text-2xl">Москва</h5>
-                        <FaTrain className="dark:text-white"/>
-                    </div>
-                    <div>
-                        <FaArrowRight className="dark:text-white"/>
-                    </div>
-                    <div className="flex flex-row gap-2 items-center">
-                        <h5 className="dark:text-white font-bold text-2xl">Пермь</h5>
-                        <FaTrain className="dark:text-white"/>
-                    </div>
-                </div>
-                <p className="dark:text-white">Отправление: 9 марта 2025 в 18:20</p>
-                <p className="dark:text-white">Прибытие: 9 марта 2025 в 18:20</p>
-                <Button>Забронировать билет</Button>
-            </Card>
+            <h1 className="dark:text-white text-center text-3xl">{route && route.length ? 'Лучший подходящий маршрут' : 'Маршрут не найден!'}</h1>
+            {
+                route && route.length ?
+                    <Card className="min-w-max" horizontal>
+                        <div className="flex flex-row gap-4 items-center">
+                            {route}
+                        </div>
+                        <Button>Забронировать билет</Button>
+                    </Card> : null
+            }
         </div>
     </>)
 }
--- init.cypher
+++ init.cypher
@@ -30,3 +30,15 @@ create (c2)-[:HAVE_ROUTE_TO{transportMode:"RAILWAY",departure:localdatetime(date
 
 match (c1), (c2) where c1.name = "Екатеринбург" and c2.name = "Пермь"
 create (c2)-[:HAVE_ROUTE_TO{transportMode:"RAILWAY",departure:localdatetime(datetime.fromepochmillis(timestamp() + (1000 * 86400))),arrival:localdatetime(datetime.fromepochmillis(timestamp() + (1000 * 86400 * 2)))}]->(c1)
+
+match (c1), (c2) where c1.name = "Тюмень" and c2.name = "Екатеринбург"
+create (c2)-[:HAVE_ROUTE_TO{transportMode:"RAILWAY",departure:localdatetime(datetime.fromepochmillis(timestamp() + (1000 * 86400))),arrival:localdatetime(datetime.fromepochmillis(timestamp() + (1000 * 86400 * 2)))}]->(c1)
+
+match (c1), (c2) where c1.name = "Екатеринбург" and c2.name = "Тюмень"
+create (c2)-[:HAVE_ROUTE_TO{transportMode:"RAILWAY",departure:localdatetime(datetime.fromepochmillis(timestamp() + (1000 * 86400))),arrival:localdatetime(datetime.fromepochmillis(timestamp() + (1000 * 86400 * 2)))}]->(c1)
+
+match (c1), (c2) where c1.name = "Екатеринбург" and c2.name = "Архангельск"
+create (c2)-[:HAVE_ROUTE_TO{transportMode:"RAILWAY",departure:localdatetime(datetime.fromepochmillis(timestamp() + (1000 * 86400 * 3))),arrival:localdatetime(datetime.fromepochmillis(timestamp() + (1000 * 86400 * 4)))}]->(c1)
+
+match (c1), (c2) where c1.name = "Архангельск" and c2.name = "Екатеринбург"
+create (c2)-[:HAVE_ROUTE_TO{transportMode:"RAILWAY",departure:localdatetime(datetime.fromepochmillis(timestamp() + (1000 * 86400 * 6))),arrival:localdatetime(datetime.fromepochmillis(timestamp() + (1000 * 86400 * 7)))}]->(c1)
